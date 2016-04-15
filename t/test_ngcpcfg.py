cd7071399f397b4737cfbc4e9b99323d0cbac774
Michael Prokop <mprokop@sipwise.com>
2016-04-15 17:21:24 +0200

    MT#16391 Initial tests using pytest
    The old testsuite wasn't updated for way too long and since
    ngcpcfg receives more and more features we need a decent test
    coverage.  pytest seems to provide the right level of
    abstraction, excellent fixtures and junit-xml reporting as
    needed.
    
    Inspired by Vincent Bernat's
    https://github.com/vincentbernat/lldpd/tree/master/tests/integration
    
    Thanks Victor Seva <vseva@sipwise.com>, Vincent Bernat <vincent@bernat.im>, Christian Hofstaedtler <christian@hofstaedtler.name> and Lukas Prokop <admin@lukas-prokop.at> for feedback, inspiration and help
    
    Change-Id: Iffed87e8cc540169bed89c00967a03e80859179e

---
+++ t/test_ngcpcfg.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env py.test-3
+
+import pytest
+import re
+import tempfile
+
+
+@pytest.mark.cmdline
+def test_ngcpcfgcfg_ok(ngcpcfgcli):
+    out = ngcpcfgcli()
+    assert 'For further usage information and options ' \
+           'see the ngcpcfg(8) man page' in out.stderr
+
+
+@pytest.mark.cmdline
+def test_ngcpcfgcfg_ko(ngcpcfgcli):
+    out = ngcpcfgcli(env={'NGCPCFG': '/doesnotexist'})
+    assert 'Error: Could not read configuration file ' \
+           '/etc/ngcp-config/ngcpcfg.cfg. Exiting.' in out.stderr
+
+
+# NOTE - this one fails if the *main* ngcpcfg.git is not
+# standing on master branch, therefore use --ignore-branch-check
+# until we've a mock/fixture for it
+@pytest.mark.mt_16391
+def test_simple_build_template_ok(ngcpcfgcli):
+    tmpdir = tempfile.mkdtemp(prefix='ngcp-', suffix='-pytest-output')
+    out = ngcpcfgcli("build", "--ignore-branch-check",
+                     "/etc/apt/apt.conf.d/71_no_recommended",
+                     env={
+                         'NGCP_PORTFILE': '/tmp/ngcpcfg.port',
+                         'OUTPUT_DIRECTORY': tmpdir,
+                         })
+    regex = re.compile(r"Generating " +
+                       tmpdir +
+                       r"//etc/apt/apt.conf.d/71_no_recommended: OK")
+    assert re.search(regex, out.stdout)
