d984cf2873ffc2e855a29f9f197560e6d8605698
Michael Prokop <mprokop@sipwise.com>
2017-05-30 18:30:19 +0200

    TT#16903 Provide tests for 'ngcpcfg set'
    Change-Id: Icd7629daf73f43fa619e41b333c1adb95a8674f3

--- t/test_ngcpcfg.py
+++ t/test_ngcpcfg.py
@@ -35,3 +35,51 @@ def test_simple_build_template_ok(ngcpcfgcli):
                        tmpdir +
                        r"//etc/apt/apt.conf.d/71_no_recommended: OK")
     assert re.search(regex, out.stdout)
+
+
+@pytest.mark.tt_16903
+def test_set_action_generate_dictionary(ngcpcfgcli, tmpdir):
+    tmpfile = tmpdir.mkdir('set_action').join("tmpfile.txt")
+    tmpfile.write("---\n")
+    out = ngcpcfgcli("set", str(tmpfile), "aaa.bbb=123")
+    assert tmpfile.read() == '''---
+aaa:
+  bbb: 123
+'''
+    assert "" in out.stdout
+    assert "" in out.stderr
+    assert out.returncode == 0
+
+
+@pytest.mark.tt_16903
+def test_set_action_generate_dictionary_subsection(ngcpcfgcli, tmpdir):
+    tmpfile = tmpdir.mkdir('set_action').join("tmpfile.txt")
+    tmpfile.write("---\n")
+    out = ngcpcfgcli("set", str(tmpfile), "aaa.bbb={'ccc','123','ddd','567'}")
+    assert tmpfile.read() == '''---
+aaa:
+  bbb:
+    ccc: '123'
+    ddd: '567'
+'''
+    assert "" in out.stdout
+    assert "" in out.stderr
+    assert out.returncode == 0
+
+
+@pytest.mark.tt_16903
+def test_set_action_generate_list(ngcpcfgcli, tmpdir):
+    tmpfile = tmpdir.mkdir('set_action').join("tmpfile.txt")
+    tmpfile.write("---\n")
+    out = ngcpcfgcli("set", str(tmpfile), "aaa.bbb=['ccc','123','ddd','567']")
+    assert tmpfile.read() == '''---
+aaa:
+  bbb:
+    - ccc
+    - '123'
+    - ddd
+    - '567'
+'''
+    assert "" in out.stdout
+    assert "" in out.stderr
+    assert out.returncode == 0
