e4f08fb0bcebcc5ead2381f827eb6339da23bc68
Victor Seva <vseva@sipwise.com>
2017-06-19 12:17:36 +0200

    TT#16316 lib: add status param
    * ['online', 'inactive'] as default
    * introduced in order to be able to filter values at templates
      so we have the ability to install new nodes without
      impacting the working system
    
    Change-Id: I78739f66f80da310347861e86b682fcf72745080

---
+++ t/test_ngcpcfg_lib_status.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env py.test-3
+
+import filecmp
+import os
+import pytest
+import re
+import tempfile
+
+
+@pytest.mark.tt_16316
+def test_status_carrier(ngcpcfgcli, tmpdir):
+    tmpdir = tempfile.mkdtemp(prefix='ngcp-', suffix='-pytest-output')
+    out = ngcpcfgcli("build", "--ignore-branch-check",
+                     "/etc/status.cfg",
+                     env={
+                         'NGCP_BASE_TT2': os.getcwd(),
+                         'NGCP_PORTFILE': '/tmp/ngcpcfg.port',
+                         'OUTPUT_DIRECTORY': tmpdir,
+                         'NGCPCFG':   'fixtures/ngcpcfg_carrier.cfg',
+                     })
+    regex = re.compile(r"Generating " +
+                       tmpdir +
+                       r"//etc/status.cfg: OK")
+    assert re.search(regex, out.stdout)
+    output_file = os.path.join(tmpdir, "etc/status.cfg")
+    test_file = "fixtures/output/status.cfg_carrier"
+    assert os.path.exists(output_file)
+    assert os.path.exists(test_file)
+    assert filecmp.cmp(output_file, test_file)
