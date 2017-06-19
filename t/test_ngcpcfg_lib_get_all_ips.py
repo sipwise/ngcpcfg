480b10a61d9bb088de6329328d9b6e5c5bda4ec9
Victor Seva <vseva@sipwise.com>
2017-06-19 12:17:36 +0200

    TT#16316 add status param
    * ['online', 'inactive'] as default
    
    Change-Id: I78739f66f80da310347861e86b682fcf72745080

--- t/test_ngcpcfg_lib_get_all_ips.py
+++ t/test_ngcpcfg_lib_get_all_ips.py
@@ -49,3 +49,25 @@ def test_all_ips_pro(ngcpcfgcli, tmpdir):
     assert os.path.exists(output_file)
     assert os.path.exists(test_file)
     assert filecmp.cmp(output_file, test_file)
+
+
+@pytest.mark.tt_16316
+def test_all_ips_carrier(ngcpcfgcli, tmpdir):
+    tmpdir = tempfile.mkdtemp(prefix='ngcp-', suffix='-pytest-output')
+    out = ngcpcfgcli("build", "--ignore-branch-check",
+                     "/etc/kamailio/lb/db/dispatcher",
+                     env={
+                         'NGCP_BASE_TT2': os.getcwd(),
+                         'NGCP_PORTFILE': '/tmp/ngcpcfg.port',
+                         'OUTPUT_DIRECTORY': tmpdir,
+                         'NGCPCFG':   'fixtures/ngcpcfg_carrier.cfg',
+                     })
+    regex = re.compile(r"Generating " +
+                       tmpdir +
+                       r"//etc/kamailio/lb/db/dispatcher: OK")
+    assert re.search(regex, out.stdout)
+    output_file = os.path.join(tmpdir, "etc/kamailio/lb/db/dispatcher")
+    test_file = "fixtures/output/dispatcher_carrier"
+    assert os.path.exists(output_file)
+    assert os.path.exists(test_file)
+    assert filecmp.cmp(output_file, test_file)
