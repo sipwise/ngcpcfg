6527208277ba2b68e6b9bdf7449a9be72be82a1b
Victor Seva <vseva@sipwise.com>
2017-07-12 12:59:09 +0200

    TT#17653 helper/tt2-daemon: support NGCP_BASE_TT2 env config
    * allows to test lib/ in a fake environment
    * add test for lib/lib_get_all_ips
    
    Change-Id: I872dcd835b73206070298a821201502c0cf7f82f

---
+++ t/test_ngcpcfg_lib_get_all_ips.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env py.test-3
+
+import pytest
+import re
+import tempfile
+import filecmp
+import os
+import os.path
+
+
+@pytest.mark.tt_17653
+def test_all_ips(ngcpcfgcli, tmpdir):
+    tmpdir = tempfile.mkdtemp(prefix='ngcp-', suffix='-pytest-output')
+    out = ngcpcfgcli("build", "--ignore-branch-check",
+                     "/etc/kamailio/lb/db/dispatcher",
+                     env={
+                         'NGCP_BASE_TT2': os.getcwd(),
+                         'NGCP_PORTFILE': '/tmp/ngcpcfg.port',
+                         'OUTPUT_DIRECTORY': tmpdir,
+                         'NGCPCFG':   'fixtures/ngcpcfg_pro.cfg',
+                     })
+    regex = re.compile(r"Generating " +
+                       tmpdir +
+                       r"//etc/kamailio/lb/db/dispatcher: OK")
+    assert re.search(regex, out.stdout)
+    output_file = os.path.join(tmpdir, "etc/kamailio/lb/db/dispatcher")
+    test_file = "fixtures/output/dispatcher"
+    assert os.path.exists(output_file)
+    assert os.path.exists(test_file)
+    assert filecmp.cmp(output_file, test_file)
