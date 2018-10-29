41f118bb4d53b3b644c306cd09b37f10cc069ea0
Manuel Montecelo <mmontecelo@sipwise.com>
2018-10-29 15:17:57 +0100

    TT#58451 Generate temporary files under /tmp
    Originally the following change, which had not been backported to mr6.5*, with
    additional fix to "t/test_ngcpcfg_build_syntax.py" to adapt for the change in
    path:
    
    TT#46601 Generate temporary files under /tmp
    
    When the "mktemp" command is invoked with a template parameter without path
    prefixes or without using "--tmpdir", it does not use "/tmp" by default, but
    instead it uses the current working directory as the place to create these
    temporary files.
    
    This was an oversight based on our side on the unknown behaviour, which was
    discovered when working on this task, because we do want to have "/tmp" as the
    home for these temporary files.
    
    Change-Id: I1b03dc90cfd8dd9364c8bc3b2bfe0e3c26979e7f
    (cherry picked from commit 20fb1d9d87e1828b42d50d8bfc6bc50a2079c39c)

--- t/test_ngcpcfg_build_syntax.py
+++ t/test_ngcpcfg_build_syntax.py
@@ -28,7 +28,7 @@ def test_bad_syntax(ngcpcfgcli, tmpdir):
 
     regex1 = re.compile(r"NOTE: Check those files for valid syntax and "
                         "encoding.*etc/bad-syntax\.txt\.tt2.*or inspecting "
-                        "temporary ngcpcfg\.bad-syntax\.txt\.")
+                        "temporary .*ngcpcfg\.bad-syntax\.txt\.")
     assert re.search(regex1, out.stdout)
 
     regex2 = re.compile(r"Error: Generating /tmp/ngcp-.*-pytest-output.*"
