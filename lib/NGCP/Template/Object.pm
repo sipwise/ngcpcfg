sipwise/ngcpcfg

commit 236b56d990dc09949a5cfb3778ea8cfdcaa0a64a

TT#43306 NGCP::Template::Object: Only match li_dist exactly
This partially reverts commit bc8ae9e795007202a3b7a645f7b1a26f0de2de8a.

We should not match li_dist with regexes as then we would trap cases
that we should not be covering by the li_dist mapping logic. Just do
exact matches.

This also fixes the hypothetical case with other regexes that are not
the wrongly specified '*'.

Change-Id: I9d9d850969e03fa9dc587f2d47a688bc0359e63c

lib/NGCP/Template/Object.pm
@@ -21,19 +21,14 @@ sub has_role
 {
     my ($self, $hostname, $role) = @_;
 
-    my $li_check = 1;
-    if ($role eq '*') {
-        $role = '.+';
-        $li_check = 0;
-    }
-
     if (not defined $self->{config}{hosts}{$hostname}) {
         $hostname = 'self';
     }
 
     # The LI role has a virtual role counterpart, which is active only in
-    # distributed mode.
-    if ($li_check && 'li_dist' =~ m/^$role$/) {
+    # distributed mode. We only check for exact matches, as we do not want
+    # to grab a regex that would match multiple cases.
+    if ('li_dist' eq $role) {
         if ($self->{config}{cluster_sets}{type} eq 'distributed') {
             $role = 'li';
         } else {
@@ -44,7 +39,7 @@ sub has_role
     if (any { m/^$role$/ } @{$self->{config}{hosts}{$hostname}{role}}) {
         # The LI roles are a bit special, they use additional keys to get
         # enabled, so we handle them here.
-        if ($li_check && 'li' =~ m/^$role$/) {
+        if ('li' =~ m/^$role$/) {
             return $self->{config}{intercept}{enable} eq 'yes';
         } else {
             # Otherwise, unconditionally enable the role.

commit e74318ddb477d2961bec6c2cda427fb6ef0a621c

TT#47820 Fix has_role handling
The li match was triggering for all wildcards and when li mode was not
enabled in some way then we were getting negative results.

The proper way to solve the original problem is to relegate the matching
of the li modes to the end, so that we can properly match on wildcards
and not interfere with other roles.

Change-Id: I7cb559a8dc10e8a832c2e2bcd78f1effc1cc101a
Fixes: commit 236b56d990dc09949a5cfb3778ea8cfdcaa0a64a

lib/NGCP/Template/Object.pm
@@ -25,26 +25,25 @@ sub has_role
         $hostname = 'self';
     }
 
-    # The LI role has a virtual role counterpart, which is active only in
-    # distributed mode. We only check for exact matches, as we do not want
-    # to grab a regex that would match multiple cases.
-    if ('li_dist' eq $role) {
-        if ($self->{config}{cluster_sets}{type} eq 'distributed') {
-            $role = 'li';
-        } else {
-            return 0;
-        }
-    }
+    my %roles = map { $_ => 1 } @{$self->{config}{hosts}{$hostname}{role}};
 
-    if (any { m/^$role$/ } @{$self->{config}{hosts}{$hostname}{role}}) {
-        # The LI roles are a bit special, they use additional keys to get
-        # enabled, so we handle them here.
-        if ('li' =~ m/^$role$/) {
-            return $self->{config}{intercept}{enable} eq 'yes';
-        } else {
-            # Otherwise, unconditionally enable the role.
-            return 1;
+    # We need to handle the LI case at the end, because it is conditional on
+    # other keys, so we cannot make it match on wildcards before the others.
+    my $has_li = exists $roles{li};
+    delete $roles{li};
+
+    return 1 if any { m/^$role$/ } keys %roles;
+
+    if ($has_li) {
+        my $li_role = 'li';
+
+        # The LI role has a virtual role counterpart, which is active only
+        # in distributed mode.
+        if ($self->{config}{cluster_sets}{type} eq 'distributed') {
+            $li_role = 'li_dist';
         }
+        return $self->{config}{intercept}{enable} eq 'yes'
+            if $li_role =~ m/^$role$/;
     }
 
     return 0;