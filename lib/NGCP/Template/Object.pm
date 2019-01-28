55c2362726046ff2c1e467584e21a4f0a33e2f97
Guillem Jover <gjover@sipwise.com>
2019-01-28 17:54:26 +0100

    TT#50701 Refactor IPv6 address expansion into an object function
    This centralizes the logic so that we have a single implementation.
    
    Change-Id: I7a8e60ddd6acde915047b4c9bfc8be737b40c884

--- lib/NGCP/Template/Object.pm
+++ lib/NGCP/Template/Object.pm
@@ -164,6 +164,20 @@ sub get_ssh_pub_key
     return $ssh_pub_key;
 }
 
+sub net_ip_expand
+{
+    my $ip = shift;
+
+    while ($ip =~ m/::/ && (() = $ip =~ m/:/g) < 8) {
+        $ip =~ s/::/::0:/;
+    }
+    $ip =~ s/::/:/;
+    $ip =~ s/^:/0:/;
+    $ip =~ s/:$/:0/;
+
+    return $ip;
+}
+
 1;
 
 __END__
@@ -228,6 +242,10 @@ Returns the NGCP management node shared name.
 
 Returns the SSH public key with $key_type ('rsa', 'ed25519', etc.) from
 the ngcpcfg shared-files storage.
+
+=item $ip = $t->net_ip_expand($ipaddr)
+
+Returns the expanded form of the IP address. Supports IPv4 and IPv6.
 
 =back
 
