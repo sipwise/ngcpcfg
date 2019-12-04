ee0802b082b1b70e28f69abb1bc000ad847b857c
Guillem Jover <gjover@sipwise.com>
2019-12-04 12:07:54 +0100

    TT#71952 Parallelize templated output generation
    Switch away from the old daemon/wrapper architecture, which complicates
    things and requires more scafolding code.
    
    We turn the daemon/wrapper and build_config into a single perl process
    that will load all YAML files, and then process each input/output pair
    on a parallel child, to try to speed up the processing as much as
    possible.
    
    For whole rebuilds, it should speed up the generation by at least a
    factor of x2.
    
    Change-Id: I51aa2f90336e34a20983d8733f45b64d9b6fea0b

--- t/test_ngcpcfg_build_syntax.py
+++ t/test_ngcpcfg_build_syntax.py
@@ -15,7 +15,6 @@ def test_bad_syntax(ngcpcfgcli, tmpdir):
                      "/etc/bad-syntax.txt",
                      env={
                          'NGCP_BASE_TT2': os.getcwd(),
-                         'NGCP_SOCKETFILE': '/tmp/ngcpcfg.socket',
                          'OUTPUT_DIRECTORY': tmpdir,
                          'NGCPCFG': 'fixtures/ngcpcfg_carrier.cfg',
                      })
@@ -26,35 +25,17 @@ def test_bad_syntax(ngcpcfgcli, tmpdir):
     print("stderr:")
     print(out.stderr.replace("\\n", "\n"))
 
-    regex1 = re.compile(r"NOTE: Check those files for valid syntax and "
-                        "encoding.*etc/bad-syntax.txt.tt2.*or inspecting "
-                        "temporary /tmp/ngcpcfg.PID[0-9]+")
-    assert re.search(regex1, out.stdout)
+    regex1 = re.compile(r"Error: Cannot process template "
+                        "'.*etc/bad-syntax.txt.tt2':.*"
+                        "file error - parse error - input file handle line 1: "
+                        "unexpected end of directive")
+    assert re.search(regex1, out.stderr)
 
     regex2 = re.compile(r"Error: Generating /tmp/ngcp-.*-pytest-output.*"
                         "/etc/bad-syntax.txt based on .*"
                         "/etc/bad-syntax.txt.tt2: FAILED")
     assert re.search(regex2, out.stderr)
 
-    regex3 = re.compile(r"Error:   file error - parse error - input file "
-                        "handle line 1: unexpected end of directive")
-    assert re.search(regex3, out.stderr)
-
     output_file = os.path.join(tmpdir, "etc/bad-syntax.txt")
-    test_file = "fixtures/output/bad-syntax.txt"
-    output_temp_file = re.sub(r".* or inspecting temporary ", "", out.stdout)
-    output_temp_file = re.sub(r"\\.*", "", output_temp_file)
-    # debug
-    print("Output temp file: '%s'" % (output_temp_file))
 
     assert not os.path.exists(output_file)
-    assert os.path.exists(output_temp_file)
-    assert os.path.exists(test_file)
-
-    # debug
-    if not filecmp.cmp(output_temp_file, test_file):
-        print("output_temp_file:")
-        print(open(output_temp_file).read())
-        print("test_file:")
-        print(open(test_file).read())
-    assert filecmp.cmp(output_temp_file, test_file)
