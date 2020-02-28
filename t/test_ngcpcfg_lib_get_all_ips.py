78b17bd19ede64f62df13f640bc51aa93bd0ea70
Guillem Jover <gjover@sipwise.com>
2020-02-28 19:50:39 +0100

    TT#76851 Extend get_all_ips test cases
    Add an entry for PRO, and add output for all IPs. This also covers the
    deduplicatioin of IPs, as on PRO and CARRIER, there is a lo interface
    per node that has the same 127.0.0.1 address.
    
    Change-Id: I1af28dcf7f9ca99ad169b2f49b4cd6f74858dcfe

--- t/test_ngcpcfg_lib_get_all_ips.py
+++ t/test_ngcpcfg_lib_get_all_ips.py
@@ -7,8 +7,29 @@ import re
 import tempfile
 
 
-@pytest.mark.tt_17653
+@pytest.mark.tt_76851
 def test_all_ips(ngcpcfgcli, tmpdir):
+    tmpdir = tempfile.mkdtemp(prefix='ngcp-', suffix='-pytest-output')
+    out = ngcpcfgcli("build", "--ignore-branch-check",
+                     "/etc/kamailio/lb/db/dispatcher",
+                     env={
+                         'NGCP_BASE_TT2': os.getcwd(),
+                         'OUTPUT_DIRECTORY': tmpdir,
+                         'NGCPCFG':   'fixtures/ngcpcfg.cfg',
+                     })
+    regex = re.compile(r"Generating " +
+                       tmpdir +
+                       r"/etc/kamailio/lb/db/dispatcher: OK")
+    assert re.search(regex, out.stdout)
+    output_file = os.path.join(tmpdir, "etc/kamailio/lb/db/dispatcher")
+    test_file = "fixtures/output/dispatcher"
+    assert os.path.exists(output_file)
+    assert os.path.exists(test_file)
+    assert filecmp.cmp(output_file, test_file)
+
+
+@pytest.mark.tt_17653
+def test_all_ips_pro(ngcpcfgcli, tmpdir):
     tmpdir = tempfile.mkdtemp(prefix='ngcp-', suffix='-pytest-output')
     out = ngcpcfgcli("build", "--ignore-branch-check",
                      "/etc/kamailio/lb/db/dispatcher",
@@ -22,7 +43,7 @@ def test_all_ips(ngcpcfgcli, tmpdir):
                        r"/etc/kamailio/lb/db/dispatcher: OK")
     assert re.search(regex, out.stdout)
     output_file = os.path.join(tmpdir, "etc/kamailio/lb/db/dispatcher")
-    test_file = "fixtures/output/dispatcher"
+    test_file = "fixtures/output/dispatcher_pro"
     assert os.path.exists(output_file)
     assert os.path.exists(test_file)
     assert filecmp.cmp(output_file, test_file)
