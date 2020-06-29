c6a557deece28b4ae4aadc18ad3b49d3d15a1af1
Rene Krenn <rkrenn@sipwise.com>
2020-06-29 18:09:54 +0200

    TT#84330 util methods to render .pem file content
    Change-Id: Idc99940d31d2eef32f44082d36dc9a59d3aeb566

--- lib/NGCP/Template/Plugin/Utils.pm
+++ lib/NGCP/Template/Plugin/Utils.pm
@@ -8,6 +8,7 @@ use parent qw(Template::Plugin);
 use MIME::Base64 ();
 use Data::Dumper ();
 use Config::General ();
+use Crypt::PK::RSA ();
 
 sub new {
     my ($class, $context, @params) = @_;
@@ -36,6 +37,24 @@ sub to_config_general {
 sub get_ref {
     my ($self, @params) = @_;
     return ref $params[0];
+}
+
+sub get_private_key_pem {
+    my ($self, @params) = @_;
+    return unless $params[0];
+    my $pk = Crypt::PK::RSA->new();
+    $pk->import_key($params[0]);
+    die('not a private key: ' . Data::Dumper::Dumper($params[0])) unless $encrytper->is_private();
+    return $pk->export_key_pem('private');
+}
+
+sub get_public_key_pem {
+    my ($self, @params) = @_;
+    return unless $params[0];
+    my $pk = Crypt::PK::RSA->new();
+    $pk->import_key($params[0]);
+    die('not a public key: ' . Data::Dumper::Dumper($params[0])) if $encrytper->is_private();
+    return $pk->export_key_pem('public');
 }
 
 1;
@@ -78,6 +97,14 @@ Serializes the given input object to string (Config::General syntax).
 =item [% Utils.get_ref(object) %]
 
 Get the variable type.
+
+=item [% pem = Utils.get_private_key_pem(private key) %]
+
+Converts the given RSA private key (JWK data structure) to .pem.
+
+=item [% pem = Utils.get_public_key_pem(public key) %]
+
+Converts the given RSA public key (JWK data structure) to .pem.
 
 =back
 
