eebcd07f17e73edf5bda71ba9de7792ed0173b9c
Victor Seva <vseva@sipwise.com>
2021-06-16 14:40:51 +0200

    TT#126250 tests for apply
    Change-Id: I3eb41c1708fcc3f9a69b504296f015b4d4dc7cc3
    (cherry picked from commit e9f0d5227d2dbafb6aadde7c965e02d061f41ad5)
    (cherry picked from commit 2ea9560c70ee45d649e7dd11f06807e2b3a21acb)

---
+++ t/test_ngcpcfg_apply.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env py.test-3
+
+import os
+import pytest
+import re
+from pathlib import Path
+
+
+@pytest.mark.apply
+def test_apply_no_commit_msg(ngcpcfgcli, tmpdir, gitrepo):
+    outdir = tmpdir.mkdir("ngcp-pytest-output")
+    src = "basic-ngcp-config.tar.gz"
+    with gitrepo.from_archive(src):
+        cfg_dir = os.path.join(gitrepo.localpath, "ngcp-config")
+        tt2_dir = os.path.join(os.getcwd(), "fixtures", "apply_templates")
+        env = {
+            "DEBUG": "true",
+            "RUN_DIR": tmpdir.mkdir("ngcp-pytest-rundir"),
+            "OUTPUT_DIRECTORY": outdir,
+            "TEMPLATE_POOL_BASE": tt2_dir,
+            # don't mess with perms
+            "SKIP_RESTORE_PERMS": "true",
+            "NGCPCTL_MAIN": cfg_dir,
+            # we just need a clean git repo
+            "NGCPCTL_BASE": cfg_dir,
+            "STATE_FILES_DIR": outdir + "/var/lib/ngcpcfg/state/",
+        }
+        # create a change in repo
+        Path(os.path.join(cfg_dir, "config.yml")).touch()
+        print("env:{}".format(env))
+        out = ngcpcfgcli(
+            "apply",
+            env=env,
+        )
+
+    msg = r"Please provide commit message"
+    regex = re.compile(msg)
+
+    # debug, only printed in logs in case of error
+    print("stdout:")
+    print(out.stdout.replace("\\n", "\n"))
+    print("stderr:")
+    print(out.stderr.replace("\\n", "\n"))
+
+    assert re.search(regex, out.stdout)
+    assert out.returncode != 0
+
+
+@pytest.mark.apply
+def test_apply_with_commit_msg(ngcpcfgcli, tmpdir, gitrepo):
+    outdir = tmpdir.mkdir("ngcp-pytest-output")
+    src = "basic-ngcp-config.tar.gz"
+    with gitrepo.from_archive(src):
+        cfg_dir = os.path.join(gitrepo.localpath, "ngcp-config")
+        tt2_dir = os.path.join(os.getcwd(), "fixtures", "apply_templates")
+        env = {
+            # "DEBUG": "true",
+            "RUN_DIR": tmpdir.mkdir("ngcp-pytest-rundir"),
+            "OUTPUT_DIRECTORY": outdir,
+            "TEMPLATE_POOL_BASE": tt2_dir,
+            # don't mess with perms
+            "SKIP_RESTORE_PERMS": "true",
+            "NGCPCTL_MAIN": cfg_dir,
+            # we just need a clean git repo
+            "NGCPCTL_BASE": cfg_dir,
+            "STATE_FILES_DIR": outdir + "/var/lib/ngcpcfg/state/",
+        }
+        print("env:{}".format(env))
+        out = ngcpcfgcli(
+            "apply",
+            "whatever commit message",
+            env=env,
+        )
+
+    msg = r"Generating .+/etc/fake.txt: OK"
+    regex = re.compile(msg)
+
+    # debug, only printed in logs in case of error
+    print("stdout:")
+    print(out.stdout.replace("\\n", "\n"))
+    print("stderr:")
+    print(out.stderr.replace("\\n", "\n"))
+
+    assert re.search(regex, out.stdout)
+    assert out.returncode == 0
