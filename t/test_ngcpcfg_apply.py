8d973a11540247d67fabd026e21bcef5357b9157
Victor Seva <vseva@sipwise.com>
2021-06-24 11:09:42 +0200

    TT#126250 Bypass 'apply' options into 'build' script
    It is necessary to allow bypassing '--ignore-shared-storage-check':
    > ngcpcfg apply --ignore-shared-storage-check
    
    Previously 'apply' options were not passing to 'build' script.
    
    * docs:
       - add all supported options in *apply*
       - move '--force-all-services' description to *services*
         and add it in the list of options
    
    Change-Id: I091142d1f74b7b9e09ce94b963ff4bf9dfaa8db7
    (cherry picked from commit eff9d4ae07fc796dbc0e6ec90ec3f1cb7178cf27)
    (cherry picked from commit a62c2baebb53c91cef994354c367eea185c897df)

--- t/test_ngcpcfg_apply.py
+++ t/test_ngcpcfg_apply.py
@@ -47,14 +47,57 @@ def test_apply_no_commit_msg(ngcpcfgcli, tmpdir, gitrepo):
 
 
 @pytest.mark.apply
-def test_apply_with_commit_msg(ngcpcfgcli, tmpdir, gitrepo):
+def test_apply_no_commit_msg_options(ngcpcfgcli, tmpdir, gitrepo):
     outdir = tmpdir.mkdir("ngcp-pytest-output")
     src = "basic-ngcp-config.tar.gz"
     with gitrepo.from_archive(src):
         cfg_dir = os.path.join(gitrepo.localpath, "ngcp-config")
         tt2_dir = os.path.join(os.getcwd(), "fixtures", "apply_templates")
         env = {
-            # "DEBUG": "true",
+            "DEBUG": "true",
+            "RUN_DIR": tmpdir.mkdir("ngcp-pytest-rundir"),
+            "OUTPUT_DIRECTORY": outdir,
+            "TEMPLATE_POOL_BASE": tt2_dir,
+            # don't mess with perms
+            "SKIP_RESTORE_PERMS": "true",
+            "NGCPCTL_MAIN": cfg_dir,
+            # we just need a clean git repo
+            "NGCPCTL_BASE": cfg_dir,
+            "STATE_FILES_DIR": outdir + "/var/lib/ngcpcfg/state/",
+        }
+        # create a change in repo
+        Path(os.path.join(cfg_dir, "config.yml")).touch()
+        print("env:{}".format(env))
+        out = ngcpcfgcli(
+            "apply",
+            "--ignore-branch-check",
+            "--dry-run",
+            env=env,
+        )
+
+    # debug, only printed in logs in case of error
+    print("stdout:")
+    print(out.stdout.replace("\\n", "\n"))
+    print("stderr:")
+    print(out.stderr.replace("\\n", "\n"))
+
+    msg = r"Please provide commit message"
+    regex = re.compile(msg)
+    assert re.search(regex, out.stdout)
+
+    assert out.returncode != 0
+
+
+@pytest.mark.apply
+def test_apply_with_commit_msg(ngcpcfgcli, tmpdir, gitrepo):
+    outdir = tmpdir.mkdir("ngcp-pytest-output")
+    src = "basic-ngcp-config.tar.gz"
+    commit_msg = "whatever commit message"
+    with gitrepo.from_archive(src):
+        cfg_dir = os.path.join(gitrepo.localpath, "ngcp-config")
+        tt2_dir = os.path.join(os.getcwd(), "fixtures", "apply_templates")
+        env = {
+            "DEBUG": "true",
             "RUN_DIR": tmpdir.mkdir("ngcp-pytest-rundir"),
             "OUTPUT_DIRECTORY": outdir,
             "TEMPLATE_POOL_BASE": tt2_dir,
@@ -68,7 +111,7 @@ def test_apply_with_commit_msg(ngcpcfgcli, tmpdir, gitrepo):
         print("env:{}".format(env))
         out = ngcpcfgcli(
             "apply",
-            "whatever commit message",
+            commit_msg,
             env=env,
         )
 
@@ -81,5 +124,63 @@ def test_apply_with_commit_msg(ngcpcfgcli, tmpdir, gitrepo):
     print("stderr:")
     print(out.stderr.replace("\\n", "\n"))
 
-    assert re.search(regex, out.stdout)
     assert out.returncode == 0
+    assert re.search(regex, out.stdout)
+
+    msg = r"DEBUG: msg:\"{}\"".format(commit_msg)
+    regex = re.compile(msg)
+    assert re.search(regex, out.stderr)
+
+
+@pytest.mark.apply
+def test_apply_with_commit_msg_options(ngcpcfgcli, tmpdir, gitrepo):
+    outdir = tmpdir.mkdir("ngcp-pytest-output")
+    src = "basic-ngcp-config.tar.gz"
+    commit_msg = "whatever commit message"
+    with gitrepo.from_archive(src):
+        cfg_dir = os.path.join(gitrepo.localpath, "ngcp-config")
+        tt2_dir = os.path.join(os.getcwd(), "fixtures", "apply_templates")
+        env = {
+            "DEBUG": "true",
+            "RUN_DIR": tmpdir.mkdir("ngcp-pytest-rundir"),
+            "OUTPUT_DIRECTORY": outdir,
+            "TEMPLATE_POOL_BASE": tt2_dir,
+            # don't mess with perms
+            "SKIP_RESTORE_PERMS": "true",
+            "NGCPCTL_MAIN": cfg_dir,
+            # we just need a clean git repo
+            "NGCPCTL_BASE": cfg_dir,
+            "STATE_FILES_DIR": outdir + "/var/lib/ngcpcfg/state/",
+        }
+        print("env:{}".format(env))
+        out = ngcpcfgcli(
+            "apply",
+            "--dry-run",
+            "--ignore-branch-check",
+            commit_msg,
+            env=env,
+        )
+
+    msg = r"Generating .+/etc/fake.txt: OK"
+    regex = re.compile(msg)
+
+    # debug, only printed in logs in case of error
+    print("stdout:")
+    print(out.stdout.replace("\\n", "\n"))
+    print("stderr:")
+    print(out.stderr.replace("\\n", "\n"))
+
+    assert out.returncode == 0
+    assert re.search(regex, out.stdout)
+
+    msg = r"DEBUG: DRYRUN = true"
+    regex = re.compile(msg)
+    assert re.search(regex, out.stderr)
+
+    msg = r"--ignore-branch-check is enabled, not checking for branch 'master'"
+    regex = re.compile(msg)
+    assert re.search(regex, out.stdout)
+
+    msg = r"DEBUG: msg:\"{}\"".format(commit_msg)
+    regex = re.compile(msg)
+    assert re.search(regex, out.stderr)
