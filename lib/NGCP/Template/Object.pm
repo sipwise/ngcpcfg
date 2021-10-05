4a94941c56ba1d6ef4cffff3b57d97ac464d6d9b
Victor Seva <vseva@sipwise.com>
2021-10-05 14:36:30 +0200

    TT#139455 lib: get_instances() add interface data from host
    * get_all_cluster_set_shared_ip_maps: add instances ips
    * add test for instances
    
    Change-Id: I68ecba09ab29618eec3962b11c2c181d920917cb

--- lib/NGCP/Template/Object.pm
+++ lib/NGCP/Template/Object.pm
@@ -196,6 +196,14 @@ sub get_instances
     my $peername = $self->{config}{hosts}{$hostname}{peer};
     foreach my $instance (@{$self->{config}{instances}}) {
         if ($instance->{host} eq $hostname or $instance->{host} eq $peername) {
+            # data coming from $hostname
+            foreach my $iface (@{$instance->{interfaces}}) {
+                my $ifname = $iface->{name};
+                $iface->{netmask} = $self->{config}{hosts}{$hostname}{$ifname}{netmask};
+                if( exists $self->{config}{hosts}{$hostname}{$ifname}{cluster_set}) {
+                    $iface->{cluster_set} = $self->{config}{hosts}{$hostname}{$ifname}{cluster_set};
+                }
+            }
             push @{$res}, $instance;
         }
     }
