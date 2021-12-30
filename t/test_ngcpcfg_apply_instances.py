686a3d765a99478d79ca266cb7b6d95fe464c582
ngcp-config <vseva@sipwise.com>
2021-12-30 16:10:44 +0100

    TT#154852 services: fix detection of changes on instance builds
    * We were missing the scenario when only the build files for an instance
      had changes. The service for that instance was not restarted
    
    Change-Id: I85ba86bc57c7fc8b6df1ac2ef1144c2d3952a4da

--- t/test_ngcpcfg_apply_instances.py
+++ t/test_ngcpcfg_apply_instances.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 import pytest
 import re
+import shutil
 from fixtures.fs import check_stdoutput
 
 
@@ -21,17 +22,90 @@ def test_apply_instances(ngcpcfgcli, tmpdir):
     )
 
     assert out.returncode == 0
-    assert re.search(r"Executing action for .*/etc/kamailio/lb/ngcpcfg.services", out.stdout)
-
     assert not re.search(r"Error", out.stdout)
     assert not re.search(r"Error", out.stderr)
 
     base_dir = out.env["OUTPUT_DIRECTORY"]
     output_file = base_dir.joinpath("ngcpcfg.service")
     check_stdoutput("INSTANCE_NAME:\n", str(output_file), tmpdir)
+    assert re.search(
+        r"Executing action for .*/etc/kamailio/lb/ngcpcfg.services", out.stdout
+    )
 
     output_file = base_dir.joinpath("ngcpcfg.serviceA")
     check_stdoutput("INSTANCE_NAME:A\n", str(output_file), tmpdir)
+    assert re.search(
+        r"Executing action for .*/etc/kamailio/lb/ngcpcfg.services\[A\]", out.stdout
+    )
 
     output_file = base_dir.joinpath("ngcpcfg.serviceB")
     check_stdoutput("INSTANCE_NAME:B\n", str(output_file), tmpdir)
+    assert re.search(
+        r"Executing action for .*/etc/kamailio/lb/ngcpcfg.services\[B\]", out.stdout
+    )
+
+
+def test_apply_instances_changes(ngcpcfg, ngcpcfgcli, tmpdir, gitrepo):
+    commit_msg = "instance changes for B"
+    env, cfg = ngcpcfg(
+        env={
+            "NGCP_HOSTNAME": "lb01a",
+            "TEMPLATE_POOL_BASE": "fixtures/apply_instances",
+            "TEMPLATE_INSTANCES": "fixtures/repos/ngcp.instances",
+            "NGCPCFG": "fixtures/ngcpcfg_carrier_instances.cfg",
+        }
+    )
+    base_dir = env["OUTPUT_DIRECTORY"]
+    base_git = Path(base_dir).joinpath("etc")
+    base_path = Path(base_dir).joinpath("etc/kamailio")
+    base_orig = Path("fixtures/output")
+    orig = [
+        ("lb_kamailio_cfg_carrier", "lb"),
+        ("lb_kamailio_cfg_instances_A", "lb_A"),
+        ("lb_kamailio_cfg_instances_B", "lb_B"),
+    ]
+    # put expected output of build_instances
+    with gitrepo.in_folder(base_git) as git:
+        git.config("user.email", "fake@pytest.fake")
+        git.config("user.name", "pytest")
+        for file, dir in orig:
+            dst_path = base_path.joinpath(dir)
+            dst_path.mkdir(parents=True, exist_ok=True)
+            dst_file = dst_path.joinpath("kamailio.cfg")
+            shutil.copy(base_orig.joinpath(file), dst_file)
+            git.add(dst_file)
+        ex, out, err = git.commit("-a", "-m", "old conf done")
+        print("{}\nstdout:\n{}stderr:{}\n".format("git commit", out, err))
+        assert ex == 0
+
+    out = ngcpcfgcli(
+        "apply",
+        commit_msg,
+        env=env,
+    )
+
+    assert out.returncode == 0
+    assert re.search(
+        r"Executing action for .*/etc/kamailio/lb/ngcpcfg.services", out.stdout
+    )
+
+    assert not re.search(r"Error", out.stdout)
+    assert not re.search(r"Error", out.stderr)
+
+    output_file = base_dir.joinpath("ngcpcfg.service")
+    assert not output_file.exists()
+    assert not re.search(
+        r"Executing action for .*/etc/kamailio/lb/ngcpcfg.services$", out.stdout
+    )
+
+    output_file = base_dir.joinpath("ngcpcfg.serviceA")
+    assert not output_file.exists()
+    assert not re.search(
+        r"Executing action for .*/etc/kamailio/lb/ngcpcfg.services\[A\]$", out.stdout
+    )
+
+    output_file = base_dir.joinpath("ngcpcfg.serviceB")
+    check_stdoutput("INSTANCE_NAME:B\n", str(output_file), tmpdir)
+    assert re.search(
+        r"Executing action for .*/etc/kamailio/lb/ngcpcfg.services\[B\]", out.stdout
+    )
