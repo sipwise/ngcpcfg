417db481a724c5bd7439be67bc7b7079d7ef5900
ngcp-config <vseva@sipwise.com>
2022-02-01 11:43:37 +0100

    TT#160856 test for lib/get_all_ips_of_connection_for_instance
    * pre-commit: define max line length
    
    Change-Id: Ie8c770bbd53cc427900cfdd44ebd6217fc885cce

---
+++ t/test_ngcpcfg_lib_get_all_ips_of_connection_for_instance.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env py.test-3
+import re
+
+from fixtures.fs import check_output
+
+msg = r"Generating {}/etc/kamailio/lb{}/whatever.cfg: OK"
+
+
+def test_carrier_instances(ngcpcfgcli):
+    out = ngcpcfgcli(
+        "build",
+        "--ignore-branch-check",
+        "/etc/kamailio/lb/whatever.cfg",
+        env={
+            "NGCP_HOSTNAME": "lb01a",
+            "TEMPLATE_POOL_BASE": "fixtures/build_instances",
+            "TEMPLATE_INSTANCES": "fixtures/repos/ngcp.instances",
+            "NGCPCFG": "fixtures/ngcpcfg_carrier_instances.cfg",
+        },
+    )
+    assert re.search(msg.format(out.env["OUTPUT_DIRECTORY"], ""), out.stdout)
+    output_file = out.env["OUTPUT_DIRECTORY"].joinpath(
+        "etc/kamailio/lb/whatever.cfg"
+    )
+    test_file = "fixtures/output/whatever.cfg_carrier"
+    check_output(str(output_file), test_file)
+
+    assert re.search(msg.format(out.env["OUTPUT_DIRECTORY"], "_A"), out.stdout)
+    output_file = out.env["OUTPUT_DIRECTORY"].joinpath(
+        "etc/kamailio/lb_A/whatever.cfg"
+    )
+    test_file = "fixtures/output/whatever.cfg_carrier_instances_A"
+    check_output(str(output_file), test_file)
+
+    assert re.search(msg.format(out.env["OUTPUT_DIRECTORY"], "_B"), out.stdout)
+    output_file = out.env["OUTPUT_DIRECTORY"].joinpath(
+        "etc/kamailio/lb_B/whatever.cfg"
+    )
+    test_file = "fixtures/output/whatever.cfg_carrier_instances_B"
+    check_output(str(output_file), test_file)
+
+
+def test_pro_instances(ngcpcfgcli):
+    out = ngcpcfgcli(
+        "build",
+        "--ignore-branch-check",
+        "/etc/kamailio/lb/whatever.cfg",
+        env={
+            "NGCP_HOSTNAME": "sp1",
+            "TEMPLATE_POOL_BASE": "fixtures/build_instances",
+            "TEMPLATE_INSTANCES": "fixtures/repos/ngcp.instances",
+            "NGCPCFG": "fixtures/ngcpcfg_pro_instances.cfg",
+        },
+    )
+    assert re.search(msg.format(out.env["OUTPUT_DIRECTORY"], ""), out.stdout)
+    output_file = out.env["OUTPUT_DIRECTORY"].joinpath(
+        "etc/kamailio/lb/whatever.cfg"
+    )
+    test_file = "fixtures/output/whatever.cfg_pro"
+    check_output(str(output_file), test_file)
+
+    assert re.search(msg.format(out.env["OUTPUT_DIRECTORY"], "_A"), out.stdout)
+    output_file = out.env["OUTPUT_DIRECTORY"].joinpath(
+        "etc/kamailio/lb_A/whatever.cfg"
+    )
+    test_file = "fixtures/output/whatever.cfg_pro_instances_A"
+    check_output(str(output_file), test_file)
+
+    assert re.search(msg.format(out.env["OUTPUT_DIRECTORY"], "_B"), out.stdout)
+    output_file = out.env["OUTPUT_DIRECTORY"].joinpath(
+        "etc/kamailio/lb_B/whatever.cfg"
+    )
+    test_file = "fixtures/output/whatever.cfg_pro_instances_B"
+    check_output(str(output_file), test_file)
